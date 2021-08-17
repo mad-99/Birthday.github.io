c31e28243a355ad85dd8167d60e77a3d08c305d8
mad-99 <madhurmaheshwari1999@gmail.com>
2021-08-17 21:46:05 +0530

    main

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://youtu.be/5rrApcG806s");
+  ifrm.setAttribute("src", "https://www.youtube.com/watch?v=OaEpjCWHY38&list=RDOaEpjCWHY38&start_radio=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
